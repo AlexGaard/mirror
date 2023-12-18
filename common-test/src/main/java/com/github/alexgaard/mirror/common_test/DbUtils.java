041592bf6b354014b0f3181de350319981912f19
Alexander Gård <alexander.olav.gaard@gmail.com>
2023-12-18 18:43:24 +0100

    Prepare for release on jitpack

--- common-test/src/main/java/com/github/alexgaard/mirror/common_test/DbUtils.java
+++ common-test/src/main/java/com/github/alexgaard/mirror/common_test/DbUtils.java
@@ -5,39 +5,19 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
-import java.io.File;
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
+import static com.github.alexgaard.mirror.common_test.InitSql.createTableDataTypesSql;
 import static com.github.alexgaard.mirror.common_test.QueryUtils.query;
 import static com.github.alexgaard.mirror.core.utils.ExceptionUtil.softenException;
 
 public class DbUtils {
 
-    public static DataSource createDataSource(PostgreSQLContainer<?> container) {
-        var config = new HikariConfig();
-        config.setJdbcUrl(container.getJdbcUrl());
-        config.setMaximumPoolSize(3);
-        config.setMinimumIdle(1);
-        config.setUsername(container.getUsername());
-        config.setPassword(container.getPassword());
-
-        return new HikariDataSource(config);
-    }
-
     public static void initTables(DataSource dataSource) {
-        executeSqlResourceScript(dataSource, "tables.sql");
-    }
-
-    public static void executeSqlResourceScript(DataSource dataSource, String resourceName) {
-       try {
-           URL resource = DbUtils.class.getClassLoader().getResource(resourceName);
-           String initSql = Files.readString(new File(resource.toURI()).toPath());
-
-           QueryUtils.update(dataSource, initSql);
-       } catch (Exception e) {
-           throw softenException(e);
-       }
+        QueryUtils.update(dataSource, createTableDataTypesSql);
     }
 
     public static void drainWalMessages(DataSource dataSource, String replicationSlotName, String publicationName) {
